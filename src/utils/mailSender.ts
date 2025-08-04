17af2a4d0d79c1379200ac8ffd4ba2560d1c4a7c
Hriday-paul <hridoychandrapaul.10@gmail.com>
2025-08-04 17:56:20 +0600

    added static port for mail sending

--- src/utils/mailSender.ts
+++ src/utils/mailSender.ts
@@ -4,19 +4,20 @@ import config from '../config';
 export const sendEmail = async (to: string, subject: string, html: string) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com.',
-    port: process.env.EMAIL_ENV === 'production' ? 465 : 587,
-    secure: process.env.EMAIL_ENV === 'production',
+    port:  465,
+    secure: true,
     auth: {
       user: config.nodemailer_host_email,
       pass: config.nodemailer_host_pass,
     },
   });
 
-  await transporter.sendMail({
+  const res = await transporter.sendMail({
     from: 'nurmdopu428@gmail.com', // sender address
     to, // list of receivers
     subject,
     text: '', // plain text body
     html, // html body
   });
+  console.log("mail res--------", res)
 };
